ebe0ce2fe19302d6ba1699b31b4ad8a01d16b1bf
JoeDezzy1 <6827851+JoeDezzy1@users.noreply.github.com>
2018-12-31 17:39:21 -0500

    Update RSPkerWrapper.java

--- orbcrafter/core/api/wrappers/character/RSPkerWrapper.java
+++ orbcrafter/core/api/wrappers/character/RSPkerWrapper.java
@@ -37,8 +37,25 @@ public class RSPkerWrapper extends PlayerWrapper
 			if (positionable == null || positionable.distanceTo(arg0) > 20)
 				return false;
 
-			final int difference = Utility.getLevelDifference(levelOpponent, playerLevel);
+			final int difference = getLevelDifference(levelOpponent, playerLevel);
 			return difference >= 0 && difference <= wildernessLevel;
 		});
 	}
+	
+	/**
+	 * Gets the combat level difference between two combat levels.
+	 *
+	 * @param cbLevelA
+	 * @param cbLevelB
+	 * @return a number >= 0 with the difference between the two levels. -1 if one or both of the input levels are < 3
+	 */
+	public static int getLevelDifference(final int cbLevelA, final int cbLevelB)
+	{
+		if (cbLevelA < 3 || cbLevelB < 3)
+			return -1;
+		if (cbLevelA > cbLevelB)
+			return cbLevelA - cbLevelB;
+		return cbLevelB - cbLevelA;
+	}
+
 }
